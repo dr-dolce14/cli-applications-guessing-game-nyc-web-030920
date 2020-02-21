f51e178877fc518555ac697ad951ecb95e0737ae
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-21 16:56:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   computer_number = rand(6) + 1
   if user_input == "exit"
     puts "Goodbye!"
-  elsif user_input == computer_number
+  else user_input == computer_number
     puts "You guessed the correct number!"
   elsif user_input != computer_number 
     puts "Sorry! The computer guessed #{computer_number}."
